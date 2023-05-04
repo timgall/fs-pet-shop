a006148b5e3a147cfdd9987424acb1e391978426
timgall <119544130+timgall@users.noreply.github.com>
2023-05-04 16:52:28 -0500

    update

--- restfulExpressServer.js
+++ restfulExpressServer.js
@@ -16,16 +16,19 @@ server.post("/pets", (req, res) => {
   console.log(req.body);
   const { age, name, type } = req.body;
   const pet = { age, name, type };
+  if (!type || !age || !name) {
+    res.sendStatus(422);
+
+    console.log(`422 error`);
+    return;
+  }
+  fs.readFile("pets.json", "UTF-8").then((data) => {
+    res.status(201).send(pet);
+  });
 });
-
-if (!type || !age || !name) {
-  res.sendStatus(422);
-  return;
-  console.log(`422 error`);
-}
-
-fs.readFile("pets.json", "UTF-8").then((data) => {
-  res.status(201).send(pet);
+server.use((req, res, next) => {
+  console.log("ERROR 404");
+  res.status(404).send("Page Not found");
 });
 
 server.listen(PORT, () => {
